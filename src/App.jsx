6ce234af5b52025f6c4f4f813bd323bcf41c3549
AdriaCParra <Costaadria524@gmail.com>
2024-03-14 18:54:15 +0100

    Add react-router-dom and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Book, FindItemGame, TapAnimalGame, Puzzle } from "./components";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="game-window">
       <div className="game-container">
-        <FindItemGame />
+        {/* <FindItemGame />
         <TapAnimalGame />
         <Book />
-        <Puzzle />
+        <Puzzle /> */}
+        <Outlet />
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Book, FindItemGame, Puzzle, TapAnimalGame } from "./components";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "book", element: <Book /> },
+      { path: "find-item-game", element: <FindItemGame /> },
+      { path: "tap-animal-game", element: <TapAnimalGame /> },
+      { path: "puzzle", element: <Puzzle /> },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
